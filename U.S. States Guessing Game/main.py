69250557058ce5e6d42e2f34470d1c1616c846e4
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2025-02-15 23:00:49 +0530

    Day 25

--- U.S. States Guessing Game/main.py
+++ U.S. States Guessing Game/main.py
@@ -23,13 +23,16 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)} / 50 states Correct", prompt="What's another state's name ?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in states_list:
-            if state not in guessed_states:
-                missing_states.append(state)
+        # missing_states = []
+        # for state in states_list:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        missing_states = [state for state in states_list if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("missed_states.csv")
         break
+
+
     if answer_state in states_list :
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -38,6 +41,7 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(float(state_data.x), float(state_data.y))
         t.write(state_data.state.item())
+
 
 
 missed_states = pandas.DataFrame
